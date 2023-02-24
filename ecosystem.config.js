048e593a325a583de763cb52e36b404e70a938f7
Vandal-William <vandal.william@hotmail.com>
2023-02-24 12:13:59 +0100

    error

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,7 @@
 module.exports = {
   apps : [{
     script: 'index.js',
-    watch: '.',
-    env_production: {
-        NODE_ENV: 'production'
-    }
-  
+    watch: '.'
   }],
 
   deploy : {
@@ -16,7 +12,7 @@ module.exports = {
       repo : 'git@github.com:Vandal-William/Orecipes-serveur.git',
       path : '/home/ubuntu/portfolio',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'npm install',
       'pre-setup': ''
     }
   }
