6316d300db4f3f6fc42e36a5d8f1948760de1b6a
Vandal-William <vandal.william@hotmail.com>
2023-02-24 11:56:15 +0100

    add production env

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps : [{
+    script: 'index.js',
+    watch: '.'
+  }],
+
+  deploy : {
+    production : {
+      user : 'ubuntu',
+      host : '51.91.251.65',
+      ref  : 'origin/main',
+      repo : 'git@github.com:Vandal-William/Orecipes-serveur.git',
+      path : '/home/ubuntu/portfolio',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
